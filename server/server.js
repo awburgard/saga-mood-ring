0684b5075888129e901746346a25e1e0d394acce
Austin Burgard <awburgard@gmail.com>
2019-05-25 08:53:34 -0500

    verifies and sets up get route for images

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const tags = require('./routes/tags.router');
+const images = require ('./routes/images.router')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 app.use('/tags', tags);
+app.use('/images', images);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
