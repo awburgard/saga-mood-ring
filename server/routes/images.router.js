f60763e1eb870a4cfd2a5874f1892f5823f43a3f
Austin Burgard <awburgard@gmail.com>
2019-05-25 09:52:53 -0500

    creates and verifies post route

--- server/routes/images.router.js
+++ server/routes/images.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     const queryString = `SELECT * FROM "tags"
                     JOIN "image_tags" ON "tags"."id"="image_tags"."tag_id"
                     JOIN "images" ON "image_tags"."image_id"="images"."id";`;
@@ -15,5 +15,19 @@ router.get('/', (req,res) => {
             res.sendStatus(500);
         })
 });
+
+router.post('/add', (req,res) => {
+    const queryString = `INSERT INTO "image_tags" ("image_id", "tag_id")
+                    VALUES ($1, $2);`;
+
+    pool.query(queryString, [req.body.imageId, req.body.tagId])
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
+});
+
 
 module.exports = router;
